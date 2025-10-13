97243a693a2b06a7466a723e71834e11ee3ce61a
Darren Delimond <darrendelimond@gmail.com>
2025-10-13 15:09:17 -0400

    =created new prodcuts and parts , also provided opitimized images so that nginx / Elastic Bean Stalk will be able to display the photos on home page

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -51,6 +51,8 @@ public class BootStrapData implements CommandLineRunner {
         }
 
 
+//        Parts
+
         if(outsourcedPartRepository.count() == 0) {
 
 //created alternator object
@@ -62,6 +64,7 @@ public class BootStrapData implements CommandLineRunner {
             alternator.setInv(30);
             alternator.setMinInv(1);
             alternator.setMaxInv(50);
+            alternator.setCompanyName("Carquest");
             outsourcedPartRepository.save(alternator);
 
 
@@ -75,7 +78,68 @@ public class BootStrapData implements CommandLineRunner {
             battery.setInv(12);
             battery.setMinInv(1);
             battery.setMaxInv(50);
+            battery.setCompanyName("DieHard");
             outsourcedPartRepository.save(battery);
+
+
+
+
+            //created serpentine object
+
+            OutsourcedPart serpentine= new OutsourcedPart();
+            serpentine.setId(7);
+            serpentine.setImagePath("belt.jpg");
+            serpentine.setName("Dayco Poly Rib Belt Serpentine Belt");
+            serpentine.setPrice(49.99);
+            serpentine.setInv(20);
+            serpentine.setMinInv(1);
+            serpentine.setMaxInv(30);
+            serpentine.setCompanyName("Dayco");
+            outsourcedPartRepository.save(serpentine);
+
+
+            //created fuelPump object
+
+            OutsourcedPart fuelPump= new OutsourcedPart();
+            fuelPump.setId(8);
+            fuelPump.setImagePath("fuelPump.jpg");
+            fuelPump.setName("Carquest Premium Mechanical Fuel Pump");
+            fuelPump.setPrice(26.99);
+            fuelPump.setInv(50);
+            fuelPump.setMinInv(10);
+            fuelPump.setMaxInv(90);
+            fuelPump.setCompanyName("Carquest");
+            outsourcedPartRepository.save(fuelPump);
+
+
+
+            //created wipers object
+
+            OutsourcedPart wipers= new OutsourcedPart();
+            wipers.setId(10);
+            wipers.setImagePath("wipers.jpg");
+            wipers.setName("Rain-X Vision Wiper Blade: 24\", Standard Beam, All Weather Performance, Pack of 1");
+            wipers.setPrice(22.99);
+            wipers.setInv(50);
+            wipers.setMinInv(1);
+            wipers.setMaxInv(100);
+            wipers.setCompanyName("Rain-X Vision");
+            outsourcedPartRepository.save(wipers);
+
+
+            //created oilFilter object
+
+            OutsourcedPart oilFilter= new OutsourcedPart();
+            oilFilter.setId(9);
+            oilFilter.setImagePath("oilFilter.jpg");
+            oilFilter.setName("Carquest Premium Oil Filter");
+            oilFilter.setPrice(9.99);
+            oilFilter.setInv(80);
+            oilFilter.setMinInv(5);
+            oilFilter.setMaxInv(100);
+            oilFilter.setCompanyName("Carquest");
+            outsourcedPartRepository.save(oilFilter);
+
         }
 
         if(inhousePartRepository.count() == 0) {
@@ -85,7 +149,7 @@ public class BootStrapData implements CommandLineRunner {
 
             InhousePart engine = new InhousePart();
             engine.setId(2);
-            engine.setImagePath("engine.webp");
+            engine.setImagePath("engine.jpg");
             engine.setName("Rylo's Premium Drive Train Engine Assembly");
             engine.setPrice(3429.99);
             engine.setInv(19);
@@ -118,6 +182,11 @@ public class BootStrapData implements CommandLineRunner {
             transmission.setMaxInv(50);
             inhousePartRepository.save(transmission);
         }
+
+
+
+
+//        Products
 
         if (productRepository.count() == 0) {
 
@@ -174,6 +243,7 @@ public class BootStrapData implements CommandLineRunner {
             //created WD-40 object
 
             Product wd40 = new Product();
+            wd40.setId(5);
             wd40.setImagePath("wd.jpg");
             wd40.setName("WD-40 Multi-Purpose Lubricant Spray - Smart Straw, 12 oz.");
             wd40.setPrice(11.99);
@@ -181,6 +251,82 @@ public class BootStrapData implements CommandLineRunner {
             wd40.setMinInv(1);
             wd40.setMaxInv(60);
             productRepository.save(wd40);
+
+
+            //created Coolant object
+
+            Product coolant = new Product();
+            coolant.setId(6);
+            coolant.setImagePath("coolant.jpg");
+            coolant.setName("Arctic Freeze Synthetic R-134a Refrigerant Kit: Includes Hose with Gauge, Seals & Delivers Colder Air");
+            coolant.setPrice(47.99);
+            coolant.setInv(10);
+            coolant.setMinInv(1);
+            coolant.setMaxInv(70);
+            productRepository.save(coolant);
+
+
+            //created transfluid object
+
+            Product transfluid = new Product();
+            transfluid.setId(7);
+            transfluid.setImagePath("transfluid.jpg");
+            transfluid.setName("Valvoline MaxLife Multi-Vehicle Automatic Transmission Fluid, 1 Gallon");
+            transfluid.setPrice(36.99);
+            transfluid.setInv(20);
+            transfluid.setMinInv(1);
+            transfluid.setMaxInv(75);
+            productRepository.save(transfluid);
+
+
+            //created powerSteering object
+            Product powerSteering = new Product();
+            powerSteering.setId(8);
+            powerSteering.setImagePath("powersteering.jpg");
+            powerSteering.setName("Prestone MAX Power Steering Fluid");
+            powerSteering.setPrice(14.99);
+            powerSteering.setInv(35);
+            powerSteering.setMinInv(8);
+            powerSteering.setMaxInv(95);
+            productRepository.save(powerSteering);
+
+            //created funnel object
+            Product funnel = new Product();
+            funnel.setId(9);
+            funnel.setImagePath("funnel.jpg");
+            funnel.setName("Performance Tool Clear View Long Reach Funnel, 1/2 Pint");
+            funnel.setPrice(2.99);
+            funnel.setInv(35);
+            funnel.setMinInv(23);
+            funnel.setMaxInv(100);
+            productRepository.save(funnel);
+
+            //created tirePump object
+            Product tirePump = new Product();
+            tirePump.setId(10);
+            tirePump.setImagePath("powersteering.jpg");
+            tirePump.setName("Slime 12V Tire Inflator: 8 Min Inflation, 0-100 PSI Dial Gauge");
+            tirePump.setPrice(34.99);
+            tirePump.setInv(57);
+            tirePump.setMinInv(13);
+            tirePump.setMaxInv(105);
+            productRepository.save(tirePump);
+
+
+            //created toolSet object
+            Product toolSet = new Product();
+            toolSet.setId(10);
+            toolSet.setImagePath("toolSet.jpg");
+            toolSet.setName("DieHard 1/4, 3/8 and 1/2 Drive Tool Set, SAE & Metric, 118-Piece, 90 Tooth");
+            toolSet.setPrice(121.99);
+            toolSet.setInv(44);
+            toolSet.setMinInv(11);
+            toolSet.setMaxInv(70);
+            productRepository.save(toolSet);
+
+
+
+
 
             System.out.println("Started in Bootstrap");
             System.out.println("Number of Products" + productRepository.count());
